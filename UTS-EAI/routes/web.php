2c8cd13e3d640e8346afffcad042752e6267c2dd
ojantopik06 <fauzantopik01@gmail.com>
2025-04-22 01:38:16 +0700

    mengedit dashboard
    melengkapi dashboard

--- UTS-EAI/routes/web.php
+++ UTS-EAI/routes/web.php
@@ -12,3 +12,7 @@ Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
 Route::get('/history', function () {
     return view('history-cinta.history');
 })->name('history.index');
+
+Route::get('/view-all-movies/{tab?}', function ($tab = 'now-showing') {
+    return view('viewAll-ojan.index', ['activeTab' => $tab]);
+})->name('movies.view-all');
